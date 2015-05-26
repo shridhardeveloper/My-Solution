ef7f2449c9d4ad46fc03958add52a8666254d50c
shridhar dev <shridhar.pasodi.developer@gmail.com>
2015-05-26 16:06:15 +0530

    adding .gitignore file

--- ConsoleApplicationDemo1/Program.cs
+++ ConsoleApplicationDemo1/Program.cs
@@ -20,7 +20,7 @@ namespace ConsoleApplicationDemo1
             Console.ReadKey();
 
             Console.WriteLine("New changes added in Branch UC2");
-            Console.WriteLine("Forcing a merge by overwriting on the same line with new code !!");
+            Console.WriteLine("Forcing a merge by overwriting on the same line with new code !! Force Merge on same line again");
             Console.WriteLine("hehehahahahahahahahahahahahaha...(devil laugh)");
             Console.ReadLine();
         }
